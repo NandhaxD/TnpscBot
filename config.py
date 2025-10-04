241c21848b6cb7b129a46d96bb72d50ea8cf8d7e
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-10-04 05:52:30 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
+
 name = 'TNPSC_ROBOT'
 bot_token = os.getenv('TOKEN', '')
-
 api_id = int(os.getenv('API_ID', 6))
 api_hash = os.getenv('API_HASH', 'eb06d4abfb49dc3eeb1aeb98ae0f581e')
 
@@ -11,10 +11,10 @@ support = 'NandhaSupport'
 updates = 'NandhaBots'
 db_channel = -100455582566
 
-dev_list = []
+dev_list = [5696053228, 5696053228]
 
 WEB_SLLEP = 4*60
-WEB_URL = ''
+WEB_URL = 'https://tnpscbot.onrender.com/'
 PORT = int(os.environ.get("PORT", 8080))
 BIND_ADDRESS = str(os.environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
 
