053538883ca0c3615f661f1bb9f43995ca5d5bac
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-10-04 06:25:15 -0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ db_channel = -100455582566
 
 dev_list = [5696053228, 5696053228]
 
-WEB_SLLEP = 4*60
+WEB_SLLEP = 3*60
 WEB_URL = 'https://tnpscbot.onrender.com/'
 PORT = int(os.environ.get("PORT", 8080))
 BIND_ADDRESS = str(os.environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
